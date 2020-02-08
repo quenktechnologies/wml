8d1d294aa10d2600d038ee2e3f96d142c2708368
Lasana Murray <lmurray@quenk.com>
2020-02-08 12:02:42 -0400

    chore: regen tests

---
+++ test/fixtures/expectations/[contract]-should-parse-constructors.ts
@@ -0,0 +1,57 @@
+import * as __wml from '../../src';
+
+
+//@ts-ignore: 6192
+import {
+Maybe as __Maybe,
+fromNullable as __fromNullable,
+fromArray as __fromArray
+}
+from '@quenk/noni/lib/data/maybe';
+//@ts-ignore:6192
+type __IfArg = ()=>__wml.Content[]
+
+//@ts-ignore:6192
+type __ForAlt = ()=> __wml.Content[]
+
+//@ts-ignore:6192
+type __ForInBody<A> =(val:A, idx:number, all:A[])=>__wml.Content[]
+
+//@ts-ignore:6192
+type __ForOfBody<A> = (val:A, key:string, all:object) =>__wml.Content[]
+
+//@ts-ignore:6192
+interface __Record<A> {
+
+ [key:string]: A
+
+}
+
+//@ts-ignore:6192
+const __if = (__expr:boolean, __conseq:__IfArg,__alt?:__IfArg) : Content[]=>
+(__expr) ? __conseq() :  __alt ? __alt() : [];
+
+//@ts-ignore:6192
+const __forIn = <A>(list:A[], f:__ForInBody<A>, alt:__ForAlt) : __wml.Content[] => {
+
+   let ret:__wml.Content[] = [];
+
+   for(let i=0; i<list.length; i++)
+       ret = ret.concat(f(list[i], i, list));
+
+   return ret.length === 0 ? alt() : ret;
+
+}
+//@ts-ignore:6192
+const __forOf = <A>(o:__Record<A>, f:__ForOfBody<A>,alt:__ForAlt) : __wml.Content[] => {
+
+    let ret:__wml.Content[] = [];
+
+    for(let key in o)
+  	    if(o.hasOwnProperty(key)) 
+	        ret = ret.concat(f((o)[key], key, o));
+
+    return ret.length === 0 ? alt(): ret;
+
+}
+export interface Test{name : string }
